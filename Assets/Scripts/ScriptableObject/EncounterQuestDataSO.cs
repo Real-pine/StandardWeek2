2214567bf48cdc4f91b5f51b6d5d7433069e2779
Real-pine <crewger9030@gmail.com>
2024-10-22 20:02:19 +0900

    Q3 구현사항1,2 구현

---
+++ Assets/Scripts/ScriptableObject/EncounterQuestDataSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EncounterQuestDataSO", menuName = "Custom/QuestData", order = 2)]
+public class EncounterQuestDataSO : QuestDataSO
+{
+    float timeLimit;
+}
---
+++ Assets/Scripts/ScriptableObject/MonsterQuestDataSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterQuestDataSO", menuName = "Custom/QuestData", order = 1)]
+public class MonsterQuestDataSO : QuestDataSO
+{
+    public int monsterKillCount;
+}
