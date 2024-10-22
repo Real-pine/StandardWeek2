20a44642022eee36047028dd3f54b397950edbec
Real-pine <crewger9030@gmail.com>
2024-10-22 19:22:58 +0900

    Q2 구현사항2 구현

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,46 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-      public GameObject prefab;
+    public GameObject arrowPrefab;
+    public GameObject monsterPrefab;
     public int poolSize = 300;
-    private List<GameObject> pool = new List<GameObject>();
+    
+    // 오브젝트 풀을 관리할 딕셔너리
+    private Dictionary<string, List<GameObject>> pools = new Dictionary<string, List<GameObject>>();
 
 
 
 
     void Start()
     {
-        // 미리 poolSize만큼 게임오브젝트를 생성한다.
-        for(int i = 0; i < poolSize; i++)
-        {
-            GameObject obj = Instantiate(prefab);
-            obj.SetActive(false);
-            pool.Add(obj);
-        }
-
-        
+        CreatePool("Arrow", arrowPrefab);
+        CreatePool("Monster",  monsterPrefab);
     }
 
-    public GameObject Get()
+    // 풀 생성 메서드
+    private void CreatePool(string key, GameObject prefab)
     {
-        // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
-       foreach(GameObject obj in pool)
+        if(!pools.ContainsKey(key))
         {
-            if(!obj.activeSelf)
+            List<GameObject> pool = new List<GameObject>();
+
+            for ( int i = 0; i < poolSize; i++ )
             {
-                obj.SetActive(true);
-                return obj;
+                GameObject obj = Instantiate(prefab);
+                obj.SetActive(false);
+                pool.Add(obj);
             }
+
+            pools.Add(key, pool);
         }
-        
     }
 
-    public void Release(GameObject obj)
+    // 꺼져있는 게임오브젝트를 찾아 activ한 상태로 변경하고 return한다.
+    public GameObject Get(string key)
+    {
+        if (pools.ContainsKey(key))
+        {
+            List< GameObject> pool = pools[key];
+
+            foreach (GameObject obj in pool )
+            {
+                if(!obj.activeSelf)
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            // 새로 생산
+            GameObject newObj = Instantiate(pool[0]);
+            newObj.SetActive(true);
+            pool.Add(newObj);
+            return newObj;
+        }
+        
+        return null; // 키값이 잘못 되었을 경우 반환
+    }
+
+    public void Release(string key, GameObject obj)
     {
         // 게임오브젝트를 deactive한다.
-        obj.SetActive(false);
+        if(pools.ContainsKey(key))
+        {
+            obj.SetActive(false);
+        }
     }
 }
